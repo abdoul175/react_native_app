fbd2ac1f37b574b1a103073b81a530c1b8cbd70b
abdoul175 <abdoulsaboursama51@gmail.com>
2024-07-15 16:01:44 +0000

    Mon premier projet React Native

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>I am so happy</Text>
+            <Text>Cool</Text>
+        </View>
+    );
+}
+
+export default Home;
